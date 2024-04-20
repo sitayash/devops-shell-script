f59d83742d34581cc608d347b6210b556c9c9d15
sitayash <vippalasita@gmail.com>
2024-04-20 16:44:39 -0500

    hello-script

--- 04-installation.sh
+++ 04-installation.sh
@@ -10,3 +10,20 @@ else
 fi
          
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "installation if mysql..FAILURE"
+    exit 1
+else
+   echo "Installation of mysql...SUCCESS"
+fi       
+
+if [ $? -ne 0 ]
+then 
+    echo "installation if git..FAILURE"
+    exit 1
+else
+   echo "Installation of git...SUCCESS"
+fi   
+echo "is script proceeding?"    
