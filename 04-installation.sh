f8132de7cb8cf86bfcaea883a876dfaa5652d6df
sitayash <vippalasita@gmail.com>
2024-04-20 17:04:51 -0500

    hello-script

--- 04-installation.sh
+++ 04-installation.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 USERID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+       echo "$2...SUCCESS"
+    fi       
+        
+
+}
+
 if  [ $USERID -ne 0 ]
 then
     echo "please run this script with root access."
