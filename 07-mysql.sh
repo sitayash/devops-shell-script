3093f0ee35b9a494a6990407a9efe9c7f552ebc1
sitayash <vippalasita@gmail.com>
2024-04-21 18:00:53 -0500

    hello-script

--- 07-mysql.sh
+++ 07-mysql.sh
@@ -35,6 +35,6 @@ VALIDATE $? "Enabling mysql server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "starting mysql server"
 
-mysql_secure_installtion --set-root-pass Expenseapp@1 &>
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>
 >$LOGFILE
 VALIDATE $? "setting up root password"
