e6a00b0c5b03461c4fe54e6d10c44924de9659ce
sitayash <vippalasita@gmail.com>
2024-04-21 17:36:23 -0500

    hello-script

--- 07-mysql.sh
+++ 07-mysql.sh
@@ -10,10 +10,10 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-       echo "$2...SUCCESS"
+       echo -e "$2...$G SUCCESS" $N"
     fi       
         
 
