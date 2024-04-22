5157e3e6ef4f78808de48515e2dc55b8a7d93c0a
sitayash <vippalasita@gmail.com>
2024-04-21 22:19:03 -0500

    hello-script

--- 08-backend.sh
+++ 08-backend.sh
@@ -58,8 +58,9 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependenci
 
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/devops-shell-script/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "copied backend service
+
 
 systemctl daemon-reload &>>$LOGFILE
 VALIDATE $? "Daemon Reload"
