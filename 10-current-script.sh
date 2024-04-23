e53701c1c509c0c3cdaddeb2221ac62f07ea929a
sitayash <vippalasita@gmail.com>
2024-04-23 17:45:12 -0500

    hello-script

--- 10-current-script.sh
+++ 10-current-script.sh
@@ -2,5 +2,5 @@
 COURSE="devops from current script"
 echo "befor callin other script, course : $course"
 echo "process instand id of  current shell script: $$"
-./11-otherscript.share
+./11-other-script.share
 echo "after calling other script, course: $COURSE"
